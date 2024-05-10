ba09983cd8d612a51259fa6155b8f65a34eb55bd
sharma01ketan <sharma01ketan@gmail.com>
2024-05-11 02:50:08 +0530

    add refreshAccessToken

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -19,7 +19,6 @@ const userSchema = new Schema(
         refreshToken: {
             type: String
         }
-
     },
     {
         timestamps: true
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 
@@ -11,5 +11,6 @@ router.route('/login').post(loginUser)
 
 //secured routes
 router.route("/logout").post(verifyJWT,logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 
 export default router
