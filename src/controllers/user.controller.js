d7b9a733f7b468916a829361a6c0ee66697ce978
sharma01ketan <sharma01ketan@gmail.com>
2024-05-10 22:33:16 +0530

    Now useable on Postman

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -17,7 +17,7 @@ const registerUser = asyncHandler( async(req,res)=>{
         throw new ApiError(400,"All Fields are Required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or:[{username}]
     })
     if(existedUser){
